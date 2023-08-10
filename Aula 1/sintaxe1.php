4148fa9da03bd1c13fefd0428cd1d192f1cace51
isasouza23 <isabellesouza659@gmail.com>
2023-08-10 13:46:00 -0300

    php exemplos

---
+++ Aula 1/sintaxe1.php
@@ -0,0 +1,9 @@
+<?php
+    //Geraçãode texto (string)
+    echo "Chama Sesi-Senai !";
+
+    /* Geração de texto estruturado (com tags, atributos) */
+    echo "<h2>Gerando <span class='destaque'>HTML</span> através do PHP.</h2>";
+    echo "<h2>Gerando <span class=\"destaque\">HTML</span> através do PHP. </h2>";
+
+    ?>
